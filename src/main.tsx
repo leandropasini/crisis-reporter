b4610713fc99da1688f12c51c468eb1a057e5bd5
Leandro Pasini <leandropasini@gmail.com>
2026-06-02 15:40:39 -0300

    feat: wire BrowserRouter routes / demo /app

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import LandingPage from "./pages/LandingPage";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/demo" element={<App mode="demo" />} />
+        <Route path="/app" element={<App mode="live" />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
